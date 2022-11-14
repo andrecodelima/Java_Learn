8d23761a617069a22cd7a8eb0ae2968584abe446
andrecodelima <andrelimag89@gmail.com>
2022-11-14 17:26:48 -0300

    concepts of encapsulation

--- POO/Estoque/src/entities/Product.java
+++ POO/Estoque/src/entities/Product.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Product {
+package entities;
+
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueInStock(){
+
+        return price * quantity;
+
+    }
+
+    public void addProducts(int quantity){
+            this.quantity += quantity;
+            //Soma o quantity que veio como argumento com o quantity
+            // da classe representado pelo this.
+    }
+
+    public void removeProducts(int quantity){
+        this.quantity -= quantity;
+    }
+
+    public String toString(){
+        return name;
+    }
 }
